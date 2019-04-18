2d281d4cd9773ad6507441a52dcc64c41b4c7847
Vonn <shevonnaboyd@gmail.com>
2019-04-17 21:46:19 -0700

    final game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,65 +1,65 @@
-var htmlContent = $("#game")
+var htmlContent = $('#game');
 var startTime = 10;
 
 var questions = [{
     question: "What is 2 + 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "4",
-    pic: "assets/images/math4"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "four",
+
 },
 {
     question: "What is 2 - 2 + 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "2",
-    pic: "assets/images/math2"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "two",
+ 
 },
 {
     question: "What is 2 x 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "4",
-    pic: "assets/images/math4"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "four",
+
 },
 {
     question: "What is 2 to the 2nd power?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "4",
-    pic: "assets/images/math4"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "four",
+
 },
 {
     question: "What is 4 + 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "6",
-    pic: "assets/images/math6"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "six",
+
 },
 {
     question: "What is 4 - 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "2",
-    pic: "assets/images/math2"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "two",
+
 },
 {
     question: "What is 4 x 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "8",
-    pic: "assets/images/math8"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "eight",
+
 },
 {
     question: "What is 4 / 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "2",
-    pic: "assets/images/math2"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "two",
+
 },
 {
     question: "What is 6 + 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "8",
-    pic: "assets/images/math8"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "eight",
+
 },
 {
     question: "What is 6 - 2?",
-    answerOptions: ["2", "4", "6", "8"],
-    answer: "4",
-    pic: "assets/images/math4"
+    answerOptions: ["two", "four", "six", "eight"],
+    answer: "four",
+
 }];
 
 var preguntas = {
@@ -68,105 +68,226 @@ var preguntas = {
     time: startTime,
     correct: 0,
     incorrect: 0,
-   
-
+  
+    
     countdown: function () {
-        trivia.time--;
-        $('#timeLeft').html(trivia.time);
+        preguntas.time--;
+        $('#secondsLeft').html(preguntas.time);
 
-        if (trivia.time === 0) {
-            trivia.timesUp();
+        if (preguntas.time === 0) {
+            preguntas.outOfTime();
         }
     },
     
     questionTime: function () {
-        timer = setInterval(trivia.time, 1000);
-        htmlContent.html("<h2>" + questions[this.currentQuestion].question + "</h2>");
-        for (var i = 0; i < questions[this.currentQuestion].answerOptions.length; i++) {
-            htmlContent.append("<button class='choices' id='button'" + questions[this.currentQuestion].answerOptions[i] + '>' + questions[this.currentQuestion].answerOptions[i] + "</button>");
+        timer = setInterval(preguntas.countdown, 1000);
+        htmlContent.html('<h2>' + questions[this.currentPregunta].question + '</h2>');
+        for (var i = 0; i < questions[this.currentPregunta].answerOptions.length; i++) {
+            htmlContent.append('<button class="choices" id="button"' + 'data-name="' + questions[this.currentPregunta].answerOptions[i] + '">' + questions[this.currentPregunta].answerOptions[i] + '</button>');
         }
     },
-    
-    nextPregunta: function () {
-        trivia.counter = timerValue;
-        $("#timeLeft").html(trivia.time);
-        trivia.currentPregunta++;
-        trivia.questionTime();
+
+    nextQuestion: function () {
+        preguntas.time = startTime;
+        $('#secondsLeft').html(preguntas.time);
+        preguntas.currentPregunta++;
+        preguntas.questionTime();
     },
-    
-    timesUp: function () {
+ 
+    outOfTime: function () {
         clearInterval(timer);
-        $('#timeLeft').html(trivia.time);
-        htmlContent.html("<h2>Times UP!!</h>");
-        htmlContent.append('<h3>The answer is: ' + questions[this.currentPregunta].answer);
-        htmlContent.append('<img src="' + questions[this.currentPregunta].pic + '" />');
-            if (trivia.currentPregunta === questions.length - 1) {
-                setTimeout(trivia.result, 1 * 1000);}
-            else {
-            setTimeout(trivia.nextPregunta, 1 * 1000);
-            }
+        $('#secondsLeft').html(preguntas.time);
+        htmlContent.html("<h2>you took too long!</h2>");
+        htmlContent.append('<h2>The answer is: ' + questions[this.currentPregunta].answer);
+
+        if (preguntas.currentPregunta === questions.length - 1) {
+            setTimeout(preguntas.result, 1 * 1000);
+        } else {
+            setTimeout(preguntas.nextQuestion, 1 * 1000);
+        }
     },
-  
-    clicked: function (x) {
+
+    selected: function (e) {
         clearInterval(timer);
         
-        if ($(x.target).data("name") === questions[this.currentQuestion].answer) {
-            this.rightAnswer();
+        if ($(e.target).data("name") === questions[this.currentPregunta].answer) {
+            this.correctlySelected();
         } else {
-            this.wrongAnswer();
+            this.incorrectlySelected();
         }
     },
     
-    rightAnswer: function () {
-        trivia.correct++;
+    correctlySelected: function () {
+        preguntas.correct++;
         clearInterval(timer);
-        htmlContent.html('<h2>' + questions[trivia.currentPregunta].answer + ' is correct!<h2>');
-        htmlContent.append('<img src="' + questions[trivia.currentPregunta].pic + '" />');
-            if (trivia.currentPregunta === questions.length - 1) {
-             setTimeout(trivia.result, 1 * 1000);}
-            else {
-            setTimeout(trivia.nextPregunta, 1 * 1000);
-            }
-        },
-    
-    wrongAnswer: function () {
-        trivia.incorrect++;
+        htmlContent.html('<h2>' + questions[preguntas.currentPregunta].answer + ' is correct!</h2>');
+        if (preguntas.currentPregunta === questions.length - 1) {
+            setTimeout(preguntas.result, 1 * 1000);
+        } else {
+            setTimeout(preguntas.nextQuestion, 1 * 1000);
+        }
+    },
+
+    incorrectlySelected: function () {
+        preguntas.incorrect++;
         clearInterval(timer);
-        htmlContent.html('<h2>Incorrect</h2>');
-        htmlContent.append('<h3>The correct answer is: ' + questions[trivia.currentPregunta].answer + '</h3>');
-        htmlContent.append('<img src="' + questions[trivia.currentPregunta].pic + '" />');
-            if (trivia.currentPregunta === questions.length - 1) {
-                setTimeout(trivia.result, 1 * 1000);
-            } else {
-                setTimeout(trivia.nextPregunta, 1 * 1000);
-            }
-        },
+        htmlContent.html('<h2>Wrong!</h2>');
+        htmlContent.append('<h2>The correct answer is: ' + questions[preguntas.currentPregunta].answer + '</h2>');
+        if (preguntas.currentPregunta === questions.length - 1) {
+            setTimeout(preguntas.result, 1 * 1000);
+        } else {
+            setTimeout(preguntas.nextQuestion, 1 * 1000);
+        }
+    },
 
      result: function () {
         clearInterval(timer);
         htmlContent.html('<h2>SCORE:</h2>');
-        $('#secondsLeft').html(trivia.time);
-        htmlContent.append('<h3>Correct: ' + trivia.correct + '</h3>');
-        htmlContent.append('<h3>Incorrect: ' + trivia.incorrect + '</h3>');
-        htmlContent.append('<br><button id="again">Play Again?</button>');
+        $('#secondsLeft').html(preguntas.time);
+        htmlContent.append('<h2>Correct: ' + preguntas.correct + '</h2>');
+        htmlContent.append('<h2>Incorrect: ' + preguntas.incorrect + '</h2>');
+        htmlContent.append('<h2>Unanswered: ' + (questions.length - (preguntas.incorrect + preguntas.correct)) + '</h2>');
+        htmlContent.append('<br><button id="replay">replay?</button>');
     },
-    
+
     reset: function () {
-        this.currentQuestion = 0;
+        this.currentPregunta = 0;
         this.time = timer;
         this.correct = 0;
         this.incorrect = 0;
-        this.questionDisplay();
+        this.questionTime();
     }
 };
 
-$(document).on('click', '#start', function (x) {
-    $('#timerDisplay').prepend('<h2>Time Remaining: <span id="secondsLeft">20</span> seconds</h2>');
-    trivia.questionTime();
+$(document).on('click', '#start', function (e) {
+    $('#clock').prepend('<h2>Time Remaining: <span id="secondsLeft">10</span> seconds</h2>');
+    preguntas.questionTime();
 });
-$(document).on('click', '#again', function (x) {
-    trivia.reset();
+$(document).on('click', '#replay', function (e) {
+    preguntas.reset();
 });
-$(document).on('click', '.choices', function (x) {
-    trivia.clicked(x);
+
+$(document).on('click', '.choices', function (e) {
+    preguntas.selected(e);
 });
+
+
+
+
+
+
+
+// var htmlContent = $("#game")
+// var startTime = 10;
+
+
+
+// var preguntas = {
+//     questions: questions,
+//     currentPregunta: 0,
+//     time: startTime,
+//     correct: 0,
+//     incorrect: 0,
+   
+
+//     countdown: function () {
+//         preguntas.time--;
+//         $('#timeLeft').html(preguntas.time);
+
+//         if (preguntas.time === 0) {
+//             preguntas.timesUp();
+//         }
+//     },
+    
+//     questionTime: function () {
+//         timer = setInterval(preguntas.time, 1000);
+//         htmlContent.html("<h2>" + questions[this.currentPreguntauestion].question + "</h2>");
+//         for (var i = 0; i < questions[this.currentPreguntauestion].answerOptions.length; i++) {
+//             htmlContent.append("<button class='choices' id='button'" + questions[this.currentPreguntauestion].answerOptions[i] + '>' + questions[this.currentPreguntauestion].answerOptions[i] + "</button>");
+//         }
+//     },
+    
+//     nextPregunta: function () {
+//         preguntas.counter = startTime;
+//         $("#timeLeft").html(preguntas.time);
+//         preguntas.currentPregunta++;
+//         preguntas.questionTime();
+//     },
+    
+//     timesUp: function () {
+//         clearInterval(timer);
+//         $('#timeLeft').html(preguntas.time);
+//         htmlContent.html("<h2>Times UP!!</h>");
+//         htmlContent.append('<h3>The answer is: ' + questions[this.currentPregunta].answer);
+//         htmlContent.append('<img src="' + questions[this.currentPregunta].pic + '" />');
+//             if (preguntas.currentPregunta === questions.length - 1) {
+//                 setTimeout(preguntas.result, 1 * 1000);}
+//             else {
+//             setTimeout(preguntas.nextPregunta, 1 * 1000);
+//             }
+//     },
+  
+//     clicked: function (x) {
+//         clearInterval(timer);
+        
+//         if ($(x.target).data("name") === questions[this.currentPreguntauestion].answer) {
+//             this.rightAnswer();
+//         } else {
+//             this.wrongAnswer();
+//         }
+//     },
+    
+//     rightAnswer: function () {
+//         preguntas.correct++;
+//         clearInterval(timer);
+//         htmlContent.html('<h2>' + questions[preguntas.currentPregunta].answer + ' is correct!<h2>');
+//         htmlContent.append('<img src="' + questions[preguntas.currentPregunta].pic + '" />');
+//             if (preguntas.currentPregunta === questions.length - 1) {
+//              setTimeout(preguntas.result, 1 * 1000);}
+//             else {
+//             setTimeout(preguntas.nextPregunta, 1 * 1000);
+//             }
+//         },
+    
+//     wrongAnswer: function () {
+//         preguntas.incorrect++;
+//         clearInterval(timer);
+//         htmlContent.html('<h2>Incorrect</h2>');
+//         htmlContent.append('<h3>The correct answer is: ' + questions[preguntas.currentPregunta].answer + '</h3>');
+//         htmlContent.append('<img src="' + questions[preguntas.currentPregunta].pic + '" />');
+//             if (preguntas.currentPregunta === questions.length - 1) {
+//                 setTimeout(preguntas.result, 1 * 1000);
+//             } else {
+//                 setTimeout(preguntas.nextPregunta, 1 * 1000);
+//             }
+//         },
+
+//      result: function () {
+//         clearInterval(timer);
+//         htmlContent.html('<h2>SCORE:</h2>');
+//         $('#secondsLeft').html(preguntas.time);
+//         htmlContent.append('<h3>Correct: ' + preguntas.correct + '</h3>');
+//         htmlContent.append('<h3>Incorrect: ' + preguntas.incorrect + '</h3>');
+//         htmlContent.append('<br><button id="again">Play Again?</button>');
+//     },
+    
+//     reset: function () {
+//         this.currentPreguntauestion = 0;
+//         this.time = timer;
+//         this.correct = 0;
+//         this.incorrect = 0;
+//         this.questionTime();
+//     }
+// };
+
+// $(document).on('click', '#start', function (x) {
+//     $('#clock').prepend('<h2>Time Remaining: <span id="secondsLeft">20</span> seconds</h2>');
+//     preguntas.questionTime();
+// });
+// $(document).on('click', '#again', function (x) {
+//     preguntas.reset();
+// });
+// $(document).on('click', '.choices', function (x) {
+//     preguntas.clicked(x);
+// });
+
